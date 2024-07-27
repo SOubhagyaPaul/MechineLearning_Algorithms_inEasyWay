5facbb3dba71a6333d46ba8cf309acdee2ee8263
SOubhagyaPaul <77911463+SOubhagyaPaul@users.noreply.github.com>
2024-07-28 02:09:17 +0530

    Create readme.sh

---
+++ neuralNetwork/readme.sh
@@ -0,0 +1,10 @@
+here Tutorial
+This tutorial is broken down into 6 parts:
+
+Initialize Network.
+Forward Propagate.
+Back Propagate Error.
+Train Network.
+Predict.
+Seeds Dataset Case Study.
+These steps will provide the foundation that you need to implement the backpropagation algorithm from scratch and apply it to your own predictive modeling problems.
